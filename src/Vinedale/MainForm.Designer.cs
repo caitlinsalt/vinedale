58c0de4f53adb0f0836ca8abfb2a80b72505299e
Will Salt <forestpines@gmail.com>
2020-01-04 22:18:48 +0000

    #4 Get UI to the point of runnability (lots of bits of the imported code currently commented out for now, but will be re-enabled later)

--- src/Vinedale/MainForm.Designer.cs
+++ src/Vinedale/MainForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Vinedale
+﻿using System;
+using Vinedale.Shell;
+
+namespace Vinedale
 {
     partial class MainForm
     {
@@ -13,9 +16,11 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing)
             {
-                components.Dispose();
+                components?.Dispose();
+                _debugOutput?.Dispose();
+                _standardOutput?.Dispose();
             }
             base.Dispose(disposing);
         }
@@ -28,12 +33,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.interpShell = new Vinedale.Shell.ShellControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.debugShell = new Vinedale.Shell.ShellControl();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.turtleWindow1 = new Vinedale.Turtle.TurtleWindow();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.interpShell);
+            this.panel1.Name = "panel1";
+            // 
+            // interpShell
+            // 
+            resources.ApplyResources(this.interpShell, "interpShell");
+            this.interpShell.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.interpShell.Name = "interpShell";
+            this.interpShell.Prompt = "? ";
+            this.interpShell.ShellTextBackgroundColour = System.Drawing.Color.DarkBlue;
+            this.interpShell.ShellTextFont = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interpShell.ShellTextForegroundColour = System.Drawing.Color.White;
+            this.interpShell.CommandEntered += new Action<object, CommandEnteredEventArgs>(this.InterpShell_CommandEntered);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.debugShell);
+            this.panel2.Controls.Add(this.label2);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // debugShell
+            // 
+            resources.ApplyResources(this.debugShell, "debugShell");
+            this.debugShell.Name = "debugShell";
+            this.debugShell.Prompt = "";
+            this.debugShell.ShellTextBackgroundColour = System.Drawing.Color.MidnightBlue;
+            this.debugShell.ShellTextFont = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debugShell.ShellTextForegroundColour = System.Drawing.Color.LightGray;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.turtleWindow1);
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Name = "panel3";
+            // 
+            // turtleWindow1
+            // 
+            this.turtleWindow1.BackColor = System.Drawing.Color.DarkBlue;
+            this.turtleWindow1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.turtleWindow1, "turtleWindow1");
+            this.turtleWindow1.Name = "turtleWindow1";
+            // 
+            // FrontEnd
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrontEnd";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Shell.ShellControl interpShell;
+        private System.Windows.Forms.Label label1;
+        private Shell.ShellControl debugShell;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private Turtle.TurtleWindow turtleWindow1;
     }
 }
--- src/Vinedale/MainForm.cs
+++ src/Vinedale/MainForm.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
+using Vinedale.Shell;
 
 namespace Vinedale
 {
     public partial class MainForm : Form
     {
+        private readonly StreamWriter _debugOutput;
+        private readonly StreamWriter _standardOutput;
+        //private Interpretor _interp;
+        //private TurtleContext _turtle;
+
         public MainForm()
         {
             InitializeComponent();
+            //_turtle = new TurtleContext();
+            //turtleWindow1.TurtleContext = _turtle;
+            _debugOutput = new StreamWriter(new ShellOutputStream(debugShell)) { AutoFlush = true };
+            //_debugOutput = new StreamWriter(new ShellOutputStream(null));
+            _standardOutput = new StreamWriter(new ShellOutputStream(interpShell)) { AutoFlush = true };
+            //_interp = new Interpretor(_standardOutput, _debugOutput, DebugMessageLevel.Logorrheic);
+            //_interp.LoadModule(new SystemCommands());
+            //_interp.LoadModule(new ShellCommands());
+            //_interp.LoadModule(new TurtleCommands(_turtle));
+            //_interp.StartInteractiveInterpretor();
+            interpShell.Enabled = true;
+            debugShell.Enabled = true;
+        }
+
+        private void InterpShell_CommandEntered(object sender, CommandEnteredEventArgs e)
+        {
+            //if (_interp != null)
+            //{
+            //    InterpretationResult result = _interp.Interpret(e.Command);
+            //    if (result == InterpretationResult.SuccessIncomplete)
+            //    {
+            //        interpShell.Prompt = "> ";
+            //    }
+            //    else
+            //    {
+            //        interpShell.Prompt = "? ";
+            //    }
+            //}
         }
     }
 }
