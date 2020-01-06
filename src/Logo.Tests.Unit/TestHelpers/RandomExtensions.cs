655dc574ab154100ba137edd9ca92b2d745e85ca
Will Salt <forestpines@gmail.com>
2020-01-06 20:22:14 +0000

    #37 Some unit tests for LogoValue

---
+++ src/Logo.Tests.Unit/TestHelpers/RandomExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Logo.Tokens;
+using System;
+
+namespace Logo.Tests.Unit.TestHelpers
+{
+    public static class RandomExtensions
+    {
+        private static LogoValueType[] _validLogoValueTypes =
+        {
+            LogoValueType.Unknown,
+            LogoValueType.Bool,
+            LogoValueType.Text,
+            LogoValueType.Number,
+            LogoValueType.Word,
+            LogoValueType.List,
+            LogoValueType.Parcel,
+        };
+
+        public static LogoValueType NextLogoValueType(this Random rnd)
+        {
+            if (rnd == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return _validLogoValueTypes[rnd.Next(_validLogoValueTypes.Length)];
+        }
+    }
+}
---
+++ src/Logo.Tests.Unit/Tokens/LogoValueUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Logo.Tests.Unit.TestHelpers;
+using Logo.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tests.Utility.Providers;
+
+namespace Logo.Tests.Unit.Tokens
+{
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+    [TestClass]
+    public class LogoValueUnitTests
+    {
+        private static readonly Random _rnd = RandomProvider.Default;
+
+        [TestMethod]
+        public void LogoValueStruct_Constructor_SetsLogoValueTypeProperty()
+        {
+            LogoValueType testParam0 = _rnd.NextLogoValueType();
+            object testParam1 = null;
+
+            LogoValue testOutput = new LogoValue(testParam0, testParam1);
+
+            Assert.AreEqual(testParam0, testOutput.Type);
+        }
+    }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+}
