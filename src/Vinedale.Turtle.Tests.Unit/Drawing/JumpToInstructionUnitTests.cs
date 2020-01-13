ae1b4bb1d7f02825f94c7b3a4ac9119760328da1
Will Salt <forestpines@gmail.com>
2020-01-13 21:40:25 +0000

    #24 Vinedale.Turtle.Drawing.JumpToInstruction now immutable.

---
+++ src/Vinedale.Turtle.Tests.Unit/Drawing/JumpToInstructionUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tests.Utility.Providers;
+using Vinedale.Turtle.Drawing;
+
+namespace Vinedale.Turtle.Tests.Unit.Drawing
+{
+    [TestClass]
+    public class JumpToInstructionUnitTests
+    {
+        private static readonly Random _rnd = RandomProvider.Default;
+
+        [TestMethod]
+        public void JumpToInstructionClass_Constructor_SetsXPropertyToValueOfFirstParameter()
+        {
+            double testParam0 = _rnd.NextDouble() * 1000;
+            double testParam1 = _rnd.NextDouble() * 1000;
+
+            JumpToInstruction testOutput = new JumpToInstruction(testParam0, testParam1);
+
+            Assert.AreEqual(testParam0, testOutput.X);
+        }
+
+        [TestMethod]
+        public void JumpToInstructionClass_Constructor_SetsYPropertyToValueOfSecondParameter()
+        {
+            double testParam0 = _rnd.NextDouble() * 1000;
+            double testParam1 = _rnd.NextDouble() * 1000;
+
+            JumpToInstruction testOutput = new JumpToInstruction(testParam0, testParam1);
+
+            Assert.AreEqual(testParam1, testOutput.Y);
+        }
+    }
+}
--- src/Vinedale.Turtle/Drawing/JumpToInstruction.cs
+++ src/Vinedale.Turtle/Drawing/JumpToInstruction.cs
@@ -8,11 +8,22 @@
         /// <summary>
         /// The absolute X-coordinate to move to.
         /// </summary>
-        public double X { get; set; }
+        public double X { get; }
 
         /// <summary>
         /// The absolute Y-coordinate to move to.
         /// </summary>
-        public double Y { get; set; }
+        public double Y { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="x">The value of the <see cref="X" /> property.</param>
+        /// <param name="y">The value of the <see cref="Y" /> property.</param>
+        public JumpToInstruction(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
