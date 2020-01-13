2c0dac0431a7e1bee74ae4b3e8d7495159963cdf
Will Salt <forestpines@gmail.com>
2020-01-13 22:14:13 +0000

    #26 Vinedale.Turtle.Drawing.LineInstruction now immutable.

---
+++ src/Vinedale.Turtle.Tests.Unit/Drawing/LineInstructionUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tests.Utility.Providers;
+using Vinedale.Turtle.Drawing;
+
+namespace Vinedale.Turtle.Tests.Unit.Drawing
+{
+    [TestClass]
+    public class LineInstructionUnitTests
+    {
+        private static readonly Random _rnd = RandomProvider.Default;
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+        [TestMethod]
+        public void LineInstructionClass_Constructor_SetsLengthPropertyToValueOfParameter()
+        {
+            double testParam0 = _rnd.NextDouble() * 1000;
+
+            LineInstruction testOutput = new LineInstruction(testParam0);
+
+            Assert.AreEqual(testParam0, testOutput.Length);
+        }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+    }
+}
