d1f658313c2ef018f8bf241e1a05c7dbab2853d9
Will Salt <forestpines@gmail.com>
2020-01-19 22:51:29 +0000

    #56 Initial unit test on OperatorToken class.

---
+++ src/Logo.Tests.Unit/Tokens/OperatorTokenUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using Logo.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tests.Utility.Providers;
+
+namespace Logo.Tests.Unit.Tokens
+{
+    [TestClass]
+    public class OperatorTokenUnitTests
+    {
+        private static readonly Random _rnd = RandomProvider.Default;
+
+        private string GetSymbol()
+        {
+            const string symbols = "+-*/";
+            int idx = _rnd.Next(symbols.Length);
+            return symbols.Substring(idx, 1);
+        }
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+        [TestMethod]
+        public void OperatorTokenClass_Constructor_SetsTextPropertyToValueOfParameter_IfParameterIsValidOperatorSymbol()
+        {
+            string testParam0 = GetSymbol();
+
+            OperatorToken testOutput = new OperatorToken(testParam0);
+
+            Assert.AreEqual(testParam0, testOutput.Text);
+        }
+
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+    }
+}
