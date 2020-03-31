79fe0bfc28b2b3074d0cbbc2fe78a6b2a322445c
Will Salt <forestpines@gmail.com>
2020-03-31 11:06:37 +0100

    #82 Implement Vinedale.Shell.ShellOutputStream.Flush()

---
+++ src/Vinedale.Tests.Unit/Shell/ShellOutputStreamUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Vinedale.Shell;
+
+namespace Vinedale.Tests.Unit.Shell
+{
+    [TestClass]
+    public class ShellOutputStreamUnitTests
+    {
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+        [TestMethod]
+        public void ShellOutputStreamClass_FlushMethod_CallsRefreshMethodOfConstructorParameter()
+        {
+            Mock<IShellControl> testParamMock = new Mock<IShellControl>();
+            using ShellOutputStream testObject = new ShellOutputStream(testParamMock.Object);
+
+            testObject.Flush();
+
+            testParamMock.Verify(m => m.Refresh(), Times.Once());
+
+        }
+
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+
+    }
+}
